2b9ddc786c8c1ba7882cba976432275befbd615c
zabipacer <zuhaib.zul@gmail.com>
2025-10-28 13:27:09 +0500

    s

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+import MohammadAliPortfolio from './components/AliPortfolio'
+
+function App() {
+ 
+
+  return (
+    <>
+<MohammadAliPortfolio/>
+
+    </>
+  )
+}
+
+export default App
